1f107dbecdd59a72c55394586435c4bb20b1cbdd
hariharan888 <hariharan@drylogics.com>
2020-10-10 15:57:21 +0530

    Updates

---
+++ precache-manifest.a2e55002705064c884a2c19cba9cf7d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b56dc15e201b05323432d44534f5e76",
+    "url": "/verse-finder/index.html"
+  },
+  {
+    "revision": "d9354a3f96edf500f365",
+    "url": "/verse-finder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f53ae609b38749e5be95",
+    "url": "/verse-finder/static/js/2.8f842e39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verse-finder/static/js/2.8f842e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9354a3f96edf500f365",
+    "url": "/verse-finder/static/js/main.7c3dfd7d.chunk.js"
+  },
+  {
+    "revision": "dfb10821c231696130ef",
+    "url": "/verse-finder/static/js/runtime-main.ce7294db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/verse-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
